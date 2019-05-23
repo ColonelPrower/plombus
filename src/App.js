808dc0dc10acf5c9102d2d33333173587fd707b5
colonelprower <alex.zurcat@gmail.com>
2019-05-22 20:13:55 -0500

    Day 2 Fixed Bulma
    seems that bulma hasn't installed properly since is a css module so I
    installed directly in the code and imported through js and react.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+//import './App.css';
+import './bulma/css/bulma.min.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
+      <div className="columns">
+        <div className="column">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="column">
+            <p className="App-intro">
+              To get started, edit <code>src/App.js</code> and save to reload.
+            </p>        
+        </div>
+        
       </div>
     );
   }
